e8b7d220d4e3a3089b1df4fd9a01aac40f0c863b
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-08-26 22:52:44 +0300

    fix: update environment variable names in app.module.ts and seed.ts for consistency

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,16 +5,19 @@ import { AppService } from "./app.service";
 import { CategoriesModule } from "./categories/categories.module";
 import { ProductsModule } from "./products/products.module";
 import { CustomersModule } from "./customers/customers.module";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: "mysql",
-      host: process.env.DB_HOST || "localhost",
-      port: +(process.env.DB_PORT || 3306),
-      username: process.env.DB_USER || "root",
-      password: process.env.DB_PASSWORD || "root",
-      database: process.env.DB_NAME || "nestdb",
+      host: process.env.DATABASE_HOST || "localhost",
+      port: +(process.env.DATABASE_PORT || 3306),
+      username: process.env.DATABASE_USERNAME || "root",
+      password: process.env.DATABASE_PASSWORD || "root",
+      database: process.env.DATABASE_NAME || "nestdb",
       autoLoadEntities: true, // automatically load all @Entity() classes
       synchronize: true, // dev only: auto create/update tables
     }),
--- src/seed.ts
+++ src/seed.ts
@@ -1,15 +1,17 @@
 import { DataSource } from "typeorm";
 import { Category } from "./categories/category.entity";
 import { Product } from "./products/product.entity";
+import * as dotenv from "dotenv";
 
-// نفس الإعدادات اللي بتحطها في TypeOrmModule
+dotenv.config();
+
 const AppDataSource = new DataSource({
   type: "mysql",
-  host: "localhost",
-  port: 3306,
-  username: "root",
-  password: "root",
-  database: "nestdb",
+  host: process.env.DB_HOST || "localhost",
+  port: +(process.env.DB_PORT || 3306),
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "nestdb",
   entities: [Category, Product],
   synchronize: true,
   dropSchema: true,
