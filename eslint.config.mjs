870b80b1ef8927b746d5829eb827445b05f0eb17
Youssef Elzedy <youssefelzedy90@gmail.com>
2025-09-05 17:37:11 +0300

    feat: add Prettier and ESLint configuration files, update package dependencies

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -33,6 +33,15 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "warn",
       "@typescript-eslint/no-unsafe-assignment": "warn",
       "@typescript-eslint/no-unsafe-return": "warn",
+      "prettier/prettier": [
+        "error",
+        {
+          trailingComma: "all",
+          singleQuote: true,
+          doubleQuote: true,
+          semi: true,
+        },
+      ],
     },
   }
 );
